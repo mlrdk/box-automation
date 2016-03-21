784b8257dc1f940d9e20e15e8296cf8389afac78
Goran Mekić <meka@tilda.center>
2016-03-21 21:39:47 +0100

    Add localhost inventory and setup ssh for root

--- run.sh
+++ run.sh
@@ -6,9 +6,11 @@ virtual_env_path=~/.virtualenvs/$virtual_env
 
 apt-get install -y sudo python-pip python-dev sshpass
 pip install --user vex
-vex --make $virtual_env pip install -U pip 
+vex --make $virtual_env pip install -U pip
 source $virtual_env_path/bin/activate
 pip install -U ansible
+sed -i 's:PermitRootLogin without-password:PermitRootLogin yes:g' /etc/ssh/sshd_config
 ansible-playbook -i provision/inventory/localhost provision/site.yml --ask-become-pass --ask-pass
+sed -i 's:PermitRootLogin yes:PermitRootLogin without-password:g' /etc/ssh/sshd_config
 
 rm -rf $virtual_env_path
