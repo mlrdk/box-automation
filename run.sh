6d2bb04e13a479ee2b17ecce59d6eea0f4be4d88
Goran Mekić <meka@tilda.center>
2016-03-26 16:28:38 +0100

    Use local connection

--- run.sh
+++ run.sh
@@ -10,10 +10,6 @@ pip install --user vex
 vex --make $virtual_env pip install -U pip
 source $virtual_env_path/bin/activate
 pip install -U ansible
-sed -i 's:PermitRootLogin without-password:PermitRootLogin yes:g' /etc/ssh/sshd_config
-systemctl restart ssh
-ansible-playbook -i provision/inventory/localhost provision/site.yml --ask-become-pass --ask-pass
-sed -i 's:PermitRootLogin yes:PermitRootLogin without-password:g' /etc/ssh/sshd_config
-systemctl restart ssh
+ansible-playbook -i provision/inventory/localhost provision/site.yml --ask-become-pass --ask-pass -c local
 
-rm -rf $virtual_env_path
+rm -rf ~/.virtualenvs
